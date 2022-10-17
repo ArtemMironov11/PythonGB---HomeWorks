1795e50be4e56e66d2737af6f09d50c5b459efdc
Tatiana Mironova <tatiananikolaeva@MacBook-Pro-Tatiana.local>
2022-10-17 23:36:03 +0300

    HW2

---
+++ HW2/task1-2hw.py
@@ -0,0 +1,24 @@
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+# *Пример:*
+
+#*  пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+from math import factorial
+
+while True:
+    try:
+        N = int(input("Введите число N: "))       
+    except ValueError:
+        print("это не целое число!")
+        continue
+    else:
+        print("Да, это целое число!")
+        break 
+
+list_N = []
+total = 1
+for i in range(1, N + 1):
+    total *= i
+    list_N.append(total)
+print(list_N)
